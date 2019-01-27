15bd49c9081003113e63a180299599d9141dc177
C00190305 <C00190305@itcarlow.ie>
2019-01-27 13:47:31 +0000

    Changed tile scale to see level easier

--- 4thYearProject/4thYearProject/WallTile.cpp
+++ 4thYearProject/4thYearProject/WallTile.cpp
@@ -4,7 +4,7 @@ WallTile::WallTile(int x, int y)
 {
 	m_texture.loadFromFile("wall.png");
 	m_sprite.setTexture(m_texture);
-	m_pos = sf::Vector2f(x * 32, y * 32);
+	m_pos = sf::Vector2f(x * 16, y * 16);
 	m_sprite.setPosition(m_pos);
 	m_sprite.setScale(0.5f, 0.5f);
 }
